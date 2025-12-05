dad3c0d10fd6d9bf41ecc1a7f3b470b05693fb60
AdityaKaashyap <adityapk333@gmail.com>
2025-12-05 11:45:20 +0530

    Initial commit for the repo

--- backend/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.api.v1 import routes_predict, routes_health, routes_users
-
-app = FastAPI(title="Smishing Detection API", version="1.0")
-
-# Include routers
-app.include_router(routes_health.router, prefix="/api/v1/health", tags=["health"])
-app.include_router(routes_predict.router, prefix="/api/v1/predict", tags=["prediction"])
-app.include_router(routes_users.router, prefix="/api/v1/users", tags=["users"])
